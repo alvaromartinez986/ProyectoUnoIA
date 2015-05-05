78c38bfdbe35ea212c20315fbbbbc389ee322be7
fermat986 <martinez.alvaro@correounivalle.edu.co>
2015-05-05 10:39:13 -0500

    profundidad con costo, y nodos creados y expand. tambien para a*

--- Logica/Asterisco.py
+++ Logica/Asterisco.py
@@ -9,57 +9,101 @@ class Asterisco:
         self.queuePrio = PriorityQueue()
         self.miAmbiente = Ambiente("archivo.txt")
         self.buscarAsterico()
+        self.nodosCread = 0
 
-
-    def getExpandiblesAsterisco(self, pos, costo):
+    #tipo de heuristica: 1 para distanciaL y 2 para distanciaLsobreCarga
+    def getExpandiblesAsterisco(self, pos, costo, carga, tipoHeur):
         #abajo, derecha, izquierda, arriba
         #arriba
         #si entrega -1 o 1, no existe la posicion o hay una pared respectivamente
         if(abs(self.miAmbiente.getPosition(pos[0]-1,pos[1]))!=1):
-            self.queuePrio.put([costo+self.miAmbiente.getCosto(pos[0]-1,pos[1]), pos[0]-1,pos[1], pos[3] + ' (' + str(pos[0]) + ',' + str(pos[1])+ ')'])
+            self.nodosCread+=1
+            if tipoHeur==1:
+                self.queuePrio.put([self.getDistanciaL(pos[0]-1,pos[1]) + costo+self.miAmbiente.getCosto(pos[0]-1,pos[1]), costo+self.miAmbiente.getCosto(pos[0]-1,pos[1]), pos[0]-1,pos[1], carga-1, pos[3] + ' (' + str(pos[0]) + ',' + str(pos[1])+ ')', pos[4]+1])
+            elif tipoHeur==2:
+                self.queuePrio.put([self.getLSobreCarga(pos[0]-1,pos[1], carga-1) + costo+self.miAmbiente.getCosto(pos[0]-1,pos[1]), costo+self.miAmbiente.getCosto(pos[0]-1,pos[1]), pos[0]-1,pos[1], carga-1, pos[3] + ' (' + str(pos[0]) + ',' + str(pos[1])+ ')', pos[4]+1])
         #izquierda
         if(abs(self.miAmbiente.getPosition(pos[0],pos[1]-1))!=1):
-            self.queuePrio.put([costo+ self.miAmbiente.getCosto(pos[0],pos[1]-1), pos[0],pos[1]-1, pos[3] + ' (' + str(pos[0]) + ',' + str(pos[1])+ ')'])
+            self.nodosCread+=1
+            if tipoHeur==1:
+                self.queuePrio.put([self.getDistanciaL(pos[0], pos[1]-1) + costo+ self.miAmbiente.getCosto(pos[0],pos[1]-1), costo+self.miAmbiente.getCosto(pos[0],pos[1]-1), pos[0],pos[1]-1, carga-1, pos[3] + ' (' + str(pos[0]) + ',' + str(pos[1])+ ')', pos[4]+1])
+            elif tipoHeur==2:
+                self.queuePrio.put([self.getLSobreCarga(pos[0], pos[1]-1, carga-1) + costo+ self.miAmbiente.getCosto(pos[0],pos[1]-1), costo+self.miAmbiente.getCosto(pos[0],pos[1]-1), pos[0],pos[1]-1, carga-1, pos[3] + ' (' + str(pos[0]) + ',' + str(pos[1])+ ')', pos[4]+1])
         #derech
         if(abs(self.miAmbiente.getPosition(pos[0],pos[1]+1))!=1):
-            self.queuePrio.put([costo+ self.miAmbiente.getCosto(pos[0],pos[1]+1), pos[0],pos[1]+1, pos[3] + ' (' + str(pos[0]) + ',' + str(pos[1])+ ')'])
+            self.nodosCread+=1
+            if tipoHeur==1:
+                self.queuePrio.put([self.getDistanciaL(pos[0], pos[1]+1) + costo+ self.miAmbiente.getCosto(pos[0],pos[1]+1), costo+self.miAmbiente.getCosto(pos[0],pos[1]+1), pos[0],pos[1]+1, carga-1, pos[3] + ' (' + str(pos[0]) + ',' + str(pos[1])+ ')', pos[4]+1])
+            elif tipoHeur==2:
+                self.queuePrio.put([self.getLSobreCarga(pos[0], pos[1]+1, carga-1) + costo+ self.miAmbiente.getCosto(pos[0],pos[1]+1), costo+self.miAmbiente.getCosto(pos[0],pos[1]+1), pos[0],pos[1]+1, carga-1, pos[3] + ' (' + str(pos[0]) + ',' + str(pos[1])+ ')', pos[4]+1])
         #abajo
         if(abs(self.miAmbiente.getPosition(pos[0]+1,pos[1]))!=1):
-            self.queuePrio.put([costo+ self.miAmbiente.getCosto(pos[0]+1,pos[1]) , pos[0]+1,pos[1], pos[3] + ' (' + str(pos[0]) + ',' + str(pos[1])+ ')'])
+            self.nodosCread+=1
+            if tipoHeur==1:
+                self.queuePrio.put([self.getDistanciaL(pos[0]+1, pos[1]) + costo+ self.miAmbiente.getCosto(pos[0]+1,pos[1]), costo+self.miAmbiente.getCosto(pos[0]+1,pos[1]), pos[0]+1,pos[1], carga-1, pos[3] + ' (' + str(pos[0]) + ',' + str(pos[1])+ ')', pos[4]+1])
+            elif tipoHeur==2:
+                self.queuePrio.put([self.getLSobreCarga(pos[0]+1, pos[1], carga-1) + costo+ self.miAmbiente.getCosto(pos[0]+1,pos[1]), costo+self.miAmbiente.getCosto(pos[0]+1,pos[1]), pos[0]+1,pos[1], carga-1, pos[3] + ' (' + str(pos[0]) + ',' + str(pos[1])+ ')', pos[4]+1])
 
     #calcula la distancia en l a la meta a partir de un punto x, y hasta una meta
-    def getDistanciaL(self, x, y, meta):
-        res  = abs(meta[0]-x)+abs(meta[1]-y)
+    def getDistanciaL(self, x, y):
+        return abs(self.meta[0]-x)+abs(self.meta[1]-y)
+
+    def getLSobreCarga(self, x, y, carga):
+        if [x,y]==self.meta:
+            res = 0 #la distancia en L es 0, por ende la division es 0
+        else:
+            if carga>0:
+                res = self.getDistanciaL(x,y)/carga #en el unico caso en que no seria admisible es cuando la carga es 0, porque la heuristica seria infinito, pero ps el costo a la meta no se cual seria como por ahi no se puede llegar a la meta
+            else:
+                res = float("inf")
         return res
 
 
     def buscarAsterico(self):
-
-
-        nivel = 0
-
         posicionActual = self.miAmbiente.posIniRobotConSln()
-        posicionMeta = self.miAmbiente.posMeta()
+        self.meta = self.miAmbiente.posMeta()
 
-        print posicionActual
+        cargaMax = 6
+        tipoHeur=1
+        if tipoHeur ==1:
+            pos_ac_costo_heur= [self.getDistanciaL(posicionActual[0], posicionActual[1])]+ [0] + posicionActual + [cargaMax] + [''] + [0]
+        elif tipoHeur ==2:
+            pos_ac_costo_heur= [self.getLSobreCarga(posicionActual[0], posicionActual[1], cargaMax)]+ [0] + posicionActual + [cargaMax] + [''] + [0]
 
-        self.queuePrio.put(posicionActual)
-        posicionActual  = posicionActual[1:]
+        self.queuePrio.put(pos_ac_costo_heur)
+        self.nodosCread+=1
+        posicionActual  = pos_ac_costo_heur[2:]
 
+        limitacion = pow(self.miAmbiente.tamano, 2)
 
-        #abajo, derecha, izquierda, arriba
-        while (posicionActual[0:2]!=posicionMeta):
-            posicionAcConCosto = self.queuePrio.get()
-            print posicionAcConCosto
-            posicionActual = posicionAcConCosto[1:]
+        nodosExp=0
 
+        while (posicionActual[0:2]!=self.meta)& (not (self.queuePrio.empty())):
+            nodosExp+=1
+            pos_ac_costo_heur = self.queuePrio.get()
+            print pos_ac_costo_heur
+            posicionActual = pos_ac_costo_heur[2:]
+            nivel = posicionActual[4]
+            carga = posicionActual[2]
 
-            self.getExpandiblesAsterisco(posicionActual, posicionAcConCosto[0])
+            if self.miAmbiente.getPosition(posicionActual[0],posicionActual[1])== 6:
+                carga =cargaMax
+
+            if (limitacion>nivel) & (carga >0):
+                self.getExpandiblesAsterisco(posicionActual, pos_ac_costo_heur[1],carga,tipoHeur)
 
 
         #la solucion se filtra en el nivel de la meta
-        print posicionActual[2].split(' ')
-        return posicionActual[2].split(' ')
+        solucion = posicionActual[3].split(' ') + [('('+ str(self.meta[0]) + ',' + str(self.meta[1]) + ')')]
+        if posicionActual[0:2]!=self.meta:
+            solucion=['']
+
+        print "nodos Creados", self.nodosCread
+        print "costo ", pos_ac_costo_heur[1]
+        print "nodos expandidos ", nodosExp
+        print "solucion ",solucion
+
+        return solucion
 
 
 obj = Asterisco()
--- Logica/Profundidad.py
+++ Logica/Profundidad.py
@@ -5,13 +5,15 @@ class Profundidad:
 
     def __init__(self):
         self.miAmbiente = Ambiente("archivo.txt")
-        self.buscarProfundidad
+        self.nodosCread = 0
+        self.buscarProfundidad()
+
 
 
 
     #posicion a expandir 
     #nivel de los nodos creados
-    def getExpandibles(self, pos, nivel, carga):
+    def getExpandibles(self, pos, nivel, carga, costo):
         #abajo, derecha, izquierda, arriba
         hijos =[]
         #arriba
@@ -19,22 +21,26 @@ class Profundidad:
         #print self.miAmbiente.getPosition(pos[0]-1,pos[1])
         #si entrega -1 o 1, no existe la posicion o hay una pared respectivamente
         if(abs(self.miAmbiente.getPosition(pos[0]-1,pos[1]))!=1):
-            hijos.append([nivel, pos[0]-1,pos[1], carga])
+            self.nodosCread+=1
+            hijos.append([nivel, pos[0]-1,pos[1], carga, costo+self.miAmbiente.getCosto(pos[0]-1,pos[1])])
         #izquierda
         #print "izq"
         #print self.miAmbiente.getPosition(pos[0],pos[1]-1)
         if(abs(self.miAmbiente.getPosition(pos[0],pos[1]-1))!=1):
-            hijos.append([nivel, pos[0],pos[1]-1, carga])
+            self.nodosCread+=1
+            hijos.append([nivel, pos[0],pos[1]-1, carga,costo+self.miAmbiente.getCosto(pos[0],pos[1]-1) ])
         #derecha
         #print "der"
         #print self.miAmbiente.getPosition(pos[0],pos[1]+1)
         if(abs(self.miAmbiente.getPosition(pos[0],pos[1]+1))!=1):
-            hijos.append([nivel, pos[0],pos[1]+1, carga])
+            self.nodosCread+=1
+            hijos.append([nivel, pos[0],pos[1]+1, carga, costo+self.miAmbiente.getCosto(pos[0],pos[1]+1)])
         #abajo
         #print "aba"
         #print self.miAmbiente.getPosition(pos[0]+1,pos[1])
         if(abs(self.miAmbiente.getPosition(pos[0]+1,pos[1]))!=1):
-            hijos.append([nivel, pos[0]+1,pos[1], carga])
+            self.nodosCread+=1
+            hijos.append([nivel, pos[0]+1,pos[1], carga, costo+self.miAmbiente.getCosto(pos[0]+1,pos[1])])
 
         return hijos
     
@@ -44,12 +50,14 @@ class Profundidad:
 
         nivel = 0
 
-        posicionActual = self.miAmbiente.posIniRobotConNivel()
+        posicionActual = self.miAmbiente.posIniRobotConNivel() + [0]
         posicionMeta = self.miAmbiente.posMeta()
         
         limitacion = pow(self.miAmbiente.tamano, 2)
 
         pila = [posicionActual]
+        self.nodosCread+=1
+        nodosExp = 0
 
         print posicionActual
         print posicionActual[1:3]
@@ -58,9 +66,12 @@ class Profundidad:
         #abajo, derecha, izquierda, arriba
         while (posicionActual[1:3]!=posicionMeta)&(len(pila)!=0):
             hijos=[]
+            nodosExp +=1
             posicionActual = pila.pop()
+
             nivel= posicionActual[0]
             carga = posicionActual[3]
+            costo = posicionActual[4]
             if self.miAmbiente.getPosition(posicionActual[1],posicionActual[2])== 6:
                 carga =6
 
@@ -71,12 +82,12 @@ class Profundidad:
 
 
             if (limitacion>nivel) & (carga >0):
-                hijos = self.getExpandibles(posicionActual[1:3], nivel+1, carga-1)
+                hijos = self.getExpandibles(posicionActual[1:3], nivel+1, carga-1, costo)
 
             if len(hijos)!=0:
                 pila.extend(hijos)
                 #print hijos
-            #print pila
+           # print pila
 
 
         if posicionActual[1:3]!=posicionMeta:
@@ -84,7 +95,10 @@ class Profundidad:
 
 
         #la solucion se filtra en el nivel de la meta
+        print "nodos Creados", self.nodosCread
+        print "costo ", posicionActual[4]
+        print "nodos expandidos ", nodosExp
         print "solucion ",solucion[0:nivel+1]
-        return solucion[0:nivel]
+        return solucion[0:nivel+1]
 
 profund = Profundidad()
